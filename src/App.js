e31a029b3df53a446a23f539426c75b0170d79cb
monaf horany <monafhorany@gmail.com>
2021-03-29 17:21:59 +0300

    29/03/2021

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const loading = (
 const TheLayout = React.lazy(() => import("./containers/TheLayout"));
 
 // Pages
-const Login = React.lazy(() => import("./views/pages/login/Login"));
+const Login = React.lazy(() => import("./views/login"));
 const Register = React.lazy(() => import("./views/pages/register/Register"));
 const Page404 = React.lazy(() => import("./views/pages/page404/Page404"));
 const Page500 = React.lazy(() => import("./views/pages/page500/Page500"));
@@ -27,7 +27,8 @@ class App extends Component {
               exact
               path="/login"
               name="Login Page"
-              render={(props) => <Login {...props} />}
+              // component={Login}
+              render={() => <Login />}
             />
             <Route
               exact
