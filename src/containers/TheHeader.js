107b172fd9edcb01601cc5078b54b6782f594f04
monaf horany <monafhorany@gmail.com>
2021-04-04 23:37:01 +0300

    04/04/2021

--- src/containers/TheHeader.js
+++ src/containers/TheHeader.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { CHeader, CToggler } from "@coreui/react";
+import { CHeader, CHeaderNav, CSubheader, CToggler } from "@coreui/react";
+import TextField from "@material-ui/core/TextField";
+import Autocomplete from "@material-ui/lab/Autocomplete";
 
 const TheHeader = () => {
   const dispatch = useDispatch();
   const sidebarShow = useSelector((state) => state.changeState.sidebarShow);
+
+  const productsList = useSelector((state) => state.productList);
+  const { products } = productsList;
 
   const toggleSidebar = () => {
     const val = [true, "responsive"].includes(sidebarShow)
@@ -19,6 +24,7 @@ const TheHeader = () => {
       : "responsive";
     dispatch({ type: "set", sidebarShow: val });
   };
+  const options = ["Option 1", "Option 2"];
 
   return (
     <CHeader withSubheader>
@@ -32,6 +38,32 @@ const TheHeader = () => {
         className="ml-3 d-md-down-none"
         onClick={toggleSidebar}
       />
+      <CHeaderNav className="d-md-down-none m-auto w-75">
+        <Autocomplete
+          size="small"
+          autoComplete
+          className="w-100"
+          getOptionLabel={(option) => option.product_en_name}
+          clearOnBlur={true}
+          clearOnEscape={true}
+          // freeSolo
+          id="free-solo-demo"
+          disableClearable
+          onChange={(event, value, reason) => console.log(event, value, reason)}
+          onClick={() => console.log("sdfsdf")}
+          // onClick={() => alert("asdasd")}
+          // onKeyDown={(e) => e.charCode === 13 && alert("asdasd")}
+          options={products}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              label="Search input"
+              margin="normal"
+              variant="outlined"
+            />
+          )}
+        />
+      </CHeaderNav>
     </CHeader>
   );
 };
